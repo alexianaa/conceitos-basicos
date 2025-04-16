4e042ec37da6b18b5bdeb0e5400399f443fdef28
Alexia Cardoso <alexiasilvacardosoo@gmail.com>
2025-04-16 11:14:50 -0300

    adiciona lista de compras

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CalculadoraComponent } from "./calculadora/calculadora.component";
+import { ListaComprasComponent } from "./lista-compras/lista-compras.component";
 
 @Component({
   selector: 'app-root',
-  imports: [CalculadoraComponent],
+  imports: [ListaComprasComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/lista-compras/lista-compras.component.ts
+++ src/app/lista-compras/lista-compras.component.ts
@@ -1,11 +1,35 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { ItemLista } from './item-lista';
 
 @Component({
   selector: 'app-lista-compras',
-  imports: [],
+  imports: [CommonModule,FormsModule],
   templateUrl: './lista-compras.component.html',
   styleUrl: './lista-compras.component.scss'
 })
 export class ListaComprasComponent {
+  lista: ItemLista[] = [];
+  item: string = '';
 
+  adicionarLista(){
+    let itemLista = new ItemLista();
+    itemLista.nome = this.item;
+    itemLista.id = this.lista.length+1;
+
+    this.lista.push(itemLista);
+
+    this.item = '';
+
+    console.table(this.lista);
+  }
+
+  riscarItem(itemLista: ItemLista){
+    itemLista.comprado = !itemLista.comprado;
+  }
+
+  limparLista() {
+    this.lista = [];
+  }
 }
